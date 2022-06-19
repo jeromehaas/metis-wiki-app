d8242be9504621eb5518aa12092ce9ed985918a4
jeromehaas <hello@jeromehaas.ch>
2022-06-19 18:13:26 +0200

    Fix: set correct path for logo in header

--- components/01-atoms/logo/logo.jsx
+++ components/01-atoms/logo/logo.jsx
@@ -1,7 +1,7 @@
 const Logo = ({ className }) => {
 
   return (
-    <img className={`logo ${className ? className : ''}`} src="media/graphics/logo-markdown-editor.svg" />
+    <img className={`logo ${className ? className : ''}`} src="/media/graphics/logo-markdown-editor.svg" />
   )
 };
 
