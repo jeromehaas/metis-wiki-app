4ded1e6e0b1f6705cf4d3694597cfcb1abf547cf
jeromehaas <hello@jeromehaas.ch>
2022-06-25 01:18:55 +0200

    Setup: meta title

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import 'styles/central.scss'
 import store from 'redux/store';
 import { Provider} from 'react-redux';
+import Head from 'next/head';
 
 function App({ Component, pageProps }) {
   return (
 		<Provider store={ store }>
+			<Head><title>Markdown Editor</title></Head>
 			<Component {...pageProps} />
 		</Provider>
 	);
