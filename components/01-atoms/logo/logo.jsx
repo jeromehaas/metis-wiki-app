19989f01edb6a750c9914e5765bde987b2cb0cd1
jeromehaas <hello@jeromehaas.ch>
2022-06-27 22:43:43 +0200

    Refactor: change logo

--- components/01-atoms/logo/logo.jsx
+++ components/01-atoms/logo/logo.jsx
@@ -1,8 +1,8 @@
 const Logo = ({ className }) => {
 
   return (
-    <img className={`logo ${className ? className : ''}`} src="/media/graphics/logo-markdown-editor.svg" />
-  )
+    <img className={`logo ${className ? className : ''}`} src="/media/graphics/logo.svg" />
+  );
 };
 
 export {
