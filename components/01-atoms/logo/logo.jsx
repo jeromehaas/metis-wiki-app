feb558c4ddf9c66cbd05037c1ee0c3c030294a4b
jeromehaas <hello@jeromehaas.ch>
2022-06-27 22:51:29 +0200

    Fix: also change the logo for the corret theme

--- components/01-atoms/logo/logo.jsx
+++ components/01-atoms/logo/logo.jsx
@@ -1,8 +1,13 @@
+import { useSelector } from 'react-redux';
+
 const Logo = ({ className }) => {
 
+	const theme = useSelector( state => state.theme.active );
+
   return (
-    <img className={`logo ${className ? className : ''}`} src="/media/graphics/logo.svg" />
+    <img className={`logo ${className ? className : ''}`} src={ theme === 'light' ? "/media/graphics/logo-light.svg" : "/media/graphics/logo-dark.svg"} />
   );
+  
 };
 
 export {
